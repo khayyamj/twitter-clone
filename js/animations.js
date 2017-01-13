c6cb2f480ae82c1a9ec2925367213932f0716f48
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-13 12:16:26 -0700

    activated Tweet button

--- js/animations.js
+++ js/animations.js
@@ -6,20 +6,28 @@ $(document).ready(function() {
       $(this).css({height:'5em'});
       $('#char-count').css('display','inline-block');
       $('#tweet-submit').css('display','inline-block');
+      $('.button').attr('disabled','disabled');
     });
 
     // As the user types the character count should decrease.
     // Once it hits 10 character or less the count should turn red
     $('.tweet-compose').on('keyup', function() {
         var $tweetCharCount = 140 - $('.tweet-compose').val().length;
-        console.log($tweetCharCount);
         $('#char-count').text($tweetCharCount);
         $tweetCharCount < 11 ? $('#char-count').css('color','red') : $('#char-count').css('color','black');
 
     // If the user puts in more than 140 characters, the tweet
     // button should be disabled (and re-enabled when there are <= 140 chars)
 
-        $tweetCharCount < 0 ? $('.button').attr('disabled','disabled') : $('.button').removeAttr('disabled');
+        $tweetCharCount < 0 || $tweetCharCount > 139 ? $('.button').attr('disabled','disabled') : $('.button').removeAttr('disabled');
 
-    })
+    });
+
+    // When user clicks "tweet" button the tweet should move to the "Tweets" column
+        $('.button').on('click', function() {
+          console.log("Tweet Button Clicked");
+
+
+        });
+
 });
