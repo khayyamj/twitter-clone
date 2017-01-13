0168f00306577c4a2b90e733b3f3c106da1ca101
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-12 17:12:21 -0700

    figures out the text box and showing char count and button for step 2

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    
+    // When the user clicks on the textarea, the textarea should double in size
+    // and the character count and Tweet buttons should be revealed.
+    $('.tweet-compose').on('click',function() {
+      $(this).css({height:'5em'});
+      $('#char-count').css('display','inline-block');
+      $('#tweet-submit').css('display','inline-block');
+    });
 
 
 });
